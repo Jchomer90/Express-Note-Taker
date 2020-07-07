bd70e56f41d337394ce7e1b9a67b35a141015fd8
Josh Homer <joshuachomer@gmail.com>
2020-07-07 16:31:00 -0400

    nested fs.writeFile inside fs.readFile, all nested in app.post

--- server.js
+++ server.js
@@ -6,29 +6,56 @@ const app = express();
 
 let PORT = process.env.PORT || 3000;
 
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(parser.json({ type: "application/**json"}));
+app.use(express.static("public"));
+
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname + "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/user/notes", function(req, res) {
-    res.sendFile(path.join(__dirname + "/db/db.json"));
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+
+    // fs.readFile(__dirname, "/db/db.json", "utf-8", function(err, data) {
+    //     if (err);
+    //         console.log("error");
+    //             let userNote = {
+    //             title: req.body.title,
+    //             text: req.body.text
+    //             };
+
+    //     let template = JSON.parse(data);
+    //     template.push(userNote);
+
+    // });
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+app.post("/api/notes", function(req, res) {
 
-app.post("/user/notes", function(req, res) {
     fs.readFile(`${__dirname}/db/db.json`, function(err, data) {
-        if (err);
-            throw err;
-    });
-    fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(JSON.parse(req)), function(err) {
-        if (err);
-            throw err;
-    })
-    console.log("success!");
+        if (err)
+            console.log(err);
+
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text
+        };
+        let template = JSON.parse(data);
+        template.push(userNote);
+    
+        fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(template), function(err) {
+            if (err) {
+                throw err;
+            }
+            console.log(template);
+        })
+    });    
 });
 
 
